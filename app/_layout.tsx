3899def02ebfc9e092fc96aa589ccf33c6bad5b6
maruthishalivahana <maruthikummari553@gmail.com>
2026-02-25 16:20:12 +0530

    added navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,14 @@ import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-  const isLoggedIn = true; // later connect to auth
+  const isLoggedIn = false; // Replace with actual authentication logic
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
       {!isLoggedIn ? (
-        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       ) : (
-        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       )}
     </Stack>
   );
