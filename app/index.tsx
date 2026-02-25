4be22132921eb0153dd31f5f6c69c6b756e6801e
Nandha050 <nandhu.4562@gmail.com>
2026-02-25 15:22:43 +0530

    intial issue fixed

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,3 @@
-import { Text, View } from "react-native";
+import App from "./App";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-800" >Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+export default App;
